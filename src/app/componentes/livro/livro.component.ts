2d1e30af09d98bbb4230608607da219eaf0d28d3
LeonSantana7 <leon.santana854@gmail.com>
2025-05-09 16:49:48 -0300

    feat: ajustes no componente livro

--- src/app/componentes/livro/livro.component.ts
+++ src/app/componentes/livro/livro.component.ts
@@ -1,15 +1,24 @@
+import { CommonModule, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-livro',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './livro.component.html',
   styleUrl: './livro.component.css'
 })
 export class LivroComponent {
+  favorito: boolean= false;
 
+  alternarFavorito(){
+    this.favorito = !this.favorito;
+  }
+  
   livro = {
     titulo: "As ondas",
-    autoria: "Virginia Woolf"
+    autoria: "Virginia Woolf",
+    imagem: "https://m.media-amazon.com/images/I/81D2d6LqZdS._AC_UF1000,1000_QL80_.jpg"
   }
+
+
 }
